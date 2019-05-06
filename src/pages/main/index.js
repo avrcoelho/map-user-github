5f80652252b6345244e235713205a2b0e8073cd1
Andre Ribeiro <andre.ribeiro@iv2.com.br>
2019-05-06 17:33:31 -0300

    add geocoder

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import MapGL, { Marker } from 'react-map-gl';
+import Geocoder from 'react-map-gl-geocoder'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 import { Creators as DevActions } from '../../stores/ducks/devs';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
+import 'react-map-gl-geocoder/dist/mapbox-gl-geocoder.css'
 
 class Main extends Component {
   state = {
@@ -17,6 +19,8 @@ class Main extends Component {
       zoom: 14,
     },
   };
+
+  mapRef = React.createRef()
 
   componentDidMount() {
     window.addEventListener('resize', this._resize);
@@ -30,6 +34,12 @@ class Main extends Component {
 
   handleHideModal = (e) => {
     const [longitude, latitude] = e.lngLat;
+
+    // verifica se encontrou o endereço
+   if(e.features[0])
+   {
+    console.log(e.features[0].properties.name)
+   }
     
     const data = {
       hideShowModal: true,
@@ -68,6 +78,25 @@ class Main extends Component {
     });
   };
 
+  handleViewportChange = (viewport) => {
+    this.setState({
+      viewport: { ...this.state.viewport, ...viewport }
+    })
+  }
+
+  handleGeocoderViewportChange = (viewport) => {
+    const geocoderDefaultOverrides = { transitionDuration: 1000 }
+ 
+    return this.handleViewportChange({
+      ...viewport,
+      ...geocoderDefaultOverrides
+    })
+  }
+
+  handleresult(e){
+    console.log(e)
+  }
+
   // handleMapClick(e) {
   //   const [latitude, longitude] = e.lngLat;
 
@@ -78,6 +107,7 @@ class Main extends Component {
     return (
       <MapGL
         {...this.state.viewport}
+        ref={this.mapRef}
         onClick={this.handleHideModal}
         mapStyle="mapbox://styles/mapbox/basic-v9"
         mapboxApiAccessToken={
@@ -85,6 +115,12 @@ class Main extends Component {
         }
         onViewportChange={viewport => this.setState({ viewport })}
       >
+      <Geocoder
+          onResult={this.handleresult}
+          mapRef={this.mapRef}
+          onViewportChange={this.handleGeocoderViewportChange}
+          mapboxApiAccessToken='pk.eyJ1IjoiYW5kcmV2cmNvZWxobyIsImEiOiJjanZiNWZ3bGkxamQ5NGFtZW9yOTU4ODY1In0.ISHC_i_ClZelfGb3KF_RCA'
+        />
       {this.props.devs.map(dev => (
           <Marker
             key={dev.id}
