1293a0d817b3a4249c721d60cac90fcd8e03e96e
Andre Coelho <avrcoelho@gmail.com>
2019-05-26 21:30:04 -0300

    tete

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Fragment } from 'react';
-import { Provider } from 'react-redux';
+import React, { Fragment } from "react";
+import { Provider } from "react-redux";
 // ele deve ser acima do store
-import './config/ReactotronConfig';
-import store from './stores';
+import "./config/ReactotronConfig";
+import store from "./stores";
 
-import Routes from './routes';
+import Routes from "./routes";
 
-import GlobalStyles from './styles/GlobalStyles';
+import GlobalStyles from "./styles/GlobalStyles";
 
 // console.tron.log('Testando');
+// pv
 
 const App = () => (
   // provider da acesso aos stores contidos na aplicação
